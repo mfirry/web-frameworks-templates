51279248d2556287ce7dc73c6eb73bd37709f337
Marco F <marco.firrincieli@unicredit.eu>
2015-02-26 09:04:01 +0100

    more on colossus

--- colossus/build.sbt
+++ colossus/build.sbt
@@ -5,5 +5,6 @@ version := "0.1.0"
 scalaVersion := "2.11.5"
 
 libraryDependencies ++= Seq(
-  "com.tumblr" %% "colossus" % "0.5.1"
+  "com.tumblr" %% "colossus" % "0.5.1",
+  "net.liftweb" %% "lift-json" % "2.6-RC1"
 )
--- colossus/src/main/scala/example/Main.scala
+++ colossus/src/main/scala/example/Main.scala
@@ -6,11 +6,17 @@ import protocols.http._
 import UrlParsing._
 import HttpMethod._
 
+import net.liftweb.json._
+import JsonDSL._
+
 object Main extends App {
 
   implicit val io_system = IOSystem()
 
   Service.become[Http]("http-echo", 9000){
-    case request @ Get on Root => request.ok("Hello world!")
+    case request @ Get on Root => {
+      val json = List(1, 2, 3)
+      request.ok(compact(render(json)))
+    }
   }
 }
