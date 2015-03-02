f87647b6300916b8d4fbe48346278f7fd090ac4d
Marco F <marco.firrincieli@unicredit.eu>
2015-03-02 10:24:43 +0100

    minor

--- colossus/src/main/scala/example/Main.scala
+++ colossus/src/main/scala/example/Main.scala
@@ -13,7 +13,7 @@ object Main extends App {
 
   implicit val io_system = IOSystem()
 
-  Service.become[Http]("http-echo", 9000){
+  Service.become[Http]("http-echo", 9000) {
     case request @ Get on Root => {
       val json = List(1, 2, 3)
       request.ok(compact(render(json)))
