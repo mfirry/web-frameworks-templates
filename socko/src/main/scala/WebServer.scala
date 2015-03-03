3a24bd0cbb02ac8613b6c00043d96eca8e20ab33
andreaP <andrea.peruffo@unicredit.eu>
2015-03-03 10:52:22 +0100

    completed socko implementation... also more idiomatic

--- socko/src/main/scala/WebServer.scala
+++ socko/src/main/scala/WebServer.scala
@@ -4,8 +4,7 @@ import org.mashupbots.socko.webserver.{WebServer => SockoWebServer}
 import org.mashupbots.socko.webserver.WebServerConfig
 import org.mashupbots.socko.events.HttpRequestEvent
 
-import akka.actor.ActorSystem
-import akka.actor.Props
+import akka.actor.{Actor, ActorSystem, Props}
 
 import org.json4s._
 import org.json4s.JsonDSL._
@@ -16,12 +15,31 @@ object WebServer extends App {
   val system = ActorSystem.create
 
   val routes = Routes({
-    case GET(request) =>
-      request match {
-        case event: HttpRequestEvent =>
-          event.response.write(compact(render(List(1,2,3))))
-      }
+    case HttpRequest(request) => request match {
+        case GET(Path("/")) =>
+          system.actorOf(Props(ListAnswarer())) ! request
+        case GET(path) => path match {
+            case PathSegments(str :: Nil) =>
+              system.actorOf(Props(StringAnswarer(str))) ! request
+            }
+       }
   })
+
+  case class ListAnswarer() extends Actor {
+    def receive = {
+      case event: HttpRequestEvent =>
+        event.response.write(compact(render(List(1,2,3))))
+        context.stop(self)
+    }
+  }
+
+  case class StringAnswarer(str: String) extends Actor {
+    def receive = {
+      case event: HttpRequestEvent =>
+        event.response.write(compact(render(str)))
+        context.stop(self)
+    }
+  }
 
   val webServer = new SockoWebServer(WebServerConfig(), routes, system)
     webServer.start()
