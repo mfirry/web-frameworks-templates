200c2f5731a257395cba1ec4f7bf63350ff09950
andreaP <andrea.peruffo@unicredit.eu>
2015-03-05 12:02:50 +0100

    added http4s

---
+++ http4s/build.sbt
@@ -0,0 +1,15 @@
+organization := "unicredit"
+
+name := "http4s-example"
+
+version := "0.0.1-SNAPSHOT"
+
+scalaVersion := "2.11.5"
+
+libraryDependencies ++= Seq(
+	"org.http4s" %% "http4s-blazeserver" % "0.6.2",
+	"org.http4s" %% "http4s-dsl" % "0.6.2",
+	"org.http4s" %% "http4s-argonaut" % "0.6.2"
+)
+
+resolvers += "Bintray" at "http://dl.bintray.com/pchiusano/maven/"
---
+++ http4s/src/main/scala/WebServer.scala
@@ -0,0 +1,23 @@
+import org.http4s._
+import org.http4s.server._
+import org.http4s.dsl._
+import _root_.argonaut._, Argonaut._
+import org.http4s.argonaut._
+
+import org.http4s.server.blaze.BlazeBuilder
+
+object WebServer extends App {
+
+	val service = HttpService {
+		case GET -> Root =>
+			Ok((List(1,2,3)).asJson)
+		case GET -> Root / str =>
+			Ok(str.asJson)
+	}
+
+	BlazeBuilder.bindHttp(8080)
+		.mountService(service, "/")
+		.run
+		.awaitShutdown()
+
+}
