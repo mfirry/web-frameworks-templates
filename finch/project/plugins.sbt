5d4067099977a3c0b0fd94f3c0aef468fb6aad31
Marco F <marco.firrincieli@unicredit.eu>
2015-06-10 09:42:47 +0200

    finch updated

--- finch/project/plugins.sbt
+++ finch/project/plugins.sbt
@@ -1,1 +1,3 @@
 addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2")
+
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.8")
--- finch/src/main/scala/WebServer.scala
+++ finch/src/main/scala/WebServer.scala
@@ -1,15 +1,33 @@
-import io.finch.micro._
-import io.finch.route.{Endpoint => _, _}
-import io.finch.json._
+import java.net.InetSocketAddress
 
+import argonaut._, Argonaut._
+
+import io.finch._
+import io.finch.{Endpoint => _, _}
+import io.finch.argonaut._
+
+import io.finch.route._
+import io.finch.route.Endpoint
+import io.finch.response._
+
+import com.twitter.finagle.Service
 import com.twitter.finagle.Httpx
+
 import com.twitter.util.Await
 
 object WebServer extends App {
 
-  val api: Endpoint =
-    Get /> Micro.value(Json.arr(1, 2, 3)) |
-    Get / string /> Micro.value
+  val json: Endpoint[HttpRequest, HttpResponse] =
+    Get / "json" /> Ok(Json("message" -> jString("Hello, World!"))).toFuture
 
-  Await.ready(Httpx.serve(":9000", api))
+  val list: Endpoint[HttpRequest, HttpResponse] =
+    Get / "list" /> Ok(Json.array(jNumber(1), jNumber(2), jNumber(3))).toFuture
+
+  val api: Service[HttpRequest, HttpResponse] = list | json
+
+  Await.ready(
+    Httpx.serve(
+      "localhost:9000", api
+    )
+  )
 }
