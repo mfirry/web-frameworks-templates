6ec927255eb20452051aa5a0b9db5fa21c051416
Marco F <marco.firrincieli@unicredit.eu>
2015-07-10 18:00:26 +0200

    + plaintext and headers

--- finch/src/main/scala/WebServer.scala
+++ finch/src/main/scala/WebServer.scala
@@ -3,7 +3,6 @@ import java.net.InetSocketAddress
 import argonaut._, Argonaut._
 
 import io.finch._
-import io.finch.argonaut._
 import io.finch.route._
 import io.finch.response._
 
@@ -12,15 +11,25 @@ import com.twitter.finagle.Httpx
 
 import com.twitter.util.Await
 
+import java.util.Calendar
+
 object WebServer extends App {
+  val ok: ResponseBuilder = Ok.withHeaders("Server" -> "finch", "Date" -> Calendar.getInstance().getTime().toString)
 
-  val json: Router[HttpResponse] =
-    Get / "json" /> Ok(Json("message" -> jString("Hello, World!")))
+  val json: Router[HttpResponse] = {
+    import io.finch.argonaut._
+    Get / "json" /> ok(Json("message" -> jString("Hello, World!")))
+  }
 
-  val list: Router[HttpResponse] =
-    Get / "list" /> Ok(List(1, 2, 3).asJson)
+  val list: Router[HttpResponse] = {
+    import io.finch.argonaut._
+    Get / "list" /> ok(List(1, 2, 3).asJson)
+  }
 
-  val api: Service[HttpRequest, HttpResponse] = (list :+: json).toService
+  val plaintext: Router[HttpResponse] =
+    Get / "plaintext" /> ok("Hello, World!")
+
+  val api: Service[HttpRequest, HttpResponse] = (list :+: json :+: plaintext).toService
 
   Await.ready(
     Httpx.serve("localhost:9000", api)
