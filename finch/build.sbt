3106a38c98648f4a483bdb9aa3c182023a2aea61
Marco F <marco.firrincieli@unicredit.eu>
2015-08-26 16:01:07 +0200

    version bump circe in, agronaut out

--- finch/build.sbt
+++ finch/build.sbt
@@ -9,8 +9,11 @@ Revolver.settings
 mainClass in (Compile, run) := Some("WebServer")
 
 libraryDependencies ++= Seq(
-    "com.github.finagle" %% "finch-core" % "0.8.0-SNAPSHOT" changing(),
-    "com.github.finagle" %% "finch-argonaut" % "0.8.0-SNAPSHOT" changing()
+    "com.github.finagle" %% "finch-core" % "0.8.0",
+    "com.github.finagle" %% "finch-circe" % "0.8.0",
+    "io.circe" %% "circe-core" % "0.1.1",
+    "io.circe" %% "circe-generic" % "0.1.1",
+    "io.circe" %% "circe-jawn" % "0.1.1"
 )
 
 resolvers += Resolver.sonatypeRepo("snapshots")
--- finch/src/main/scala/WebServer.scala
+++ finch/src/main/scala/WebServer.scala
@@ -1,13 +1,12 @@
 import java.net.InetSocketAddress
 
-import argonaut._, Argonaut._
-
 import io.finch._
 import io.finch.route._
 import io.finch.response._
 
 import com.twitter.finagle.Service
 import com.twitter.finagle.Httpx
+import com.twitter.finagle.httpx.{Response, Request}
 
 import com.twitter.util.Await
 
@@ -16,20 +15,33 @@ import java.util.Calendar
 object WebServer extends App {
   val ok: ResponseBuilder = Ok.withHeaders("Server" -> "finch", "Date" -> Calendar.getInstance().getTime().toString)
 
-  val json: Router[HttpResponse] = {
-    import io.finch.argonaut._
-    Get / "json" /> ok(Json("message" -> jString("Hello, World!")))
+  val json = get("json") {
+    import io.circe._
+    import io.circe.generic.auto._
+    import io.circe.jawn._
+    import io.circe.syntax._
+    import io.finch.response.EncodeResponse
+    import io.circe.{Decoder, Encoder, Json}
+    import io.finch.circe._
+
+    case class Message(message: String)
+    Ok(Message("Hello, World!").asJson)
   }
 
-  val list: Router[HttpResponse] = {
-    import io.finch.argonaut._
-    Get / "list" /> ok(List(1, 2, 3).asJson)
+  val plaintext = get("plaintext") { "Hello, World!" }
+
+  val list = get("list") {
+    import io.circe._
+    import io.circe.generic.auto._
+    import io.circe.jawn._
+    import io.circe.syntax._
+    import io.finch.response.EncodeResponse
+    import io.circe.{Decoder, Encoder, Json}
+    import io.finch.circe._
+    Ok( List(1, 2, 3).asJson )
   }
 
-  val plaintext: Router[HttpResponse] =
-    Get / "plaintext" /> ok("Hello, World!")
-
-  val api: Service[HttpRequest, HttpResponse] = (list :+: json :+: plaintext).toService
+  val api: Service[Request, Response] = (plaintext :+: list :+: json).toService
 
   Await.ready(
     Httpx.serve("localhost:9000", api)
