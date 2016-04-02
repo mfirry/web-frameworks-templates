be95800c5086cb646db6f2e47b34ddf9de960622
Marco F <mfirry@gmail.com>
2016-04-02 16:09:05 +0200

    scalatra version bump

--- scalatra/src/main/scala/com/example/app/MyScalatraServlet.scala
+++ scalatra/src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -1,11 +1,11 @@
 package com.example.app
 
-import org.scalatra._
 import org.json4s.{DefaultFormats, Formats}
+import org.scalatra._
 import org.scalatra.json._
 
 class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
-  protected implicit val jsonFormats: Formats = DefaultFormats
+  protected implicit lazy val jsonFormats: org.json4s.Formats = org.json4s.DefaultFormats
 
   before() {
     contentType = formats("json")
