9f3ecc700756733fcb75b1cd78df2b2295457901
Marco F <mfirry@gmail.com>
2016-10-23 16:24:21 +0200

    scalatra -> 2.4.1

--- scalatra/project/build.scala
+++ scalatra/project/build.scala
@@ -11,7 +11,7 @@ object MyScalatraWebAppBuild extends Build {
   val Name = "My Scalatra Web App"
   val Version = "0.1.0-SNAPSHOT"
   val ScalaVersion = "2.11.8"
-  val ScalatraVersion = "2.4.0"
+  val ScalatraVersion = "2.4.1"
 
   lazy val project = Project (
     "my-scalatra-web-app",
@@ -22,15 +22,29 @@ object MyScalatraWebAppBuild extends Build {
       version := Version,
       scalaVersion := ScalaVersion,
       resolvers += Classpaths.typesafeReleases,
+      resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
       libraryDependencies ++= Seq(
         "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.json4s"   %% "json4s-jackson" % "3.3.0",
         "org.scalatra" %% "scalatra-json" % ScalatraVersion,
+        "org.json4s"   %% "json4s-jackson" % "3.3.0",
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
+        "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
-        "javax.servlet" % "javax.servlet-api" % "3.1.0"
-      )
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
+      ),
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
+        Seq(
+          TemplateConfig(
+            base / "webapp" / "WEB-INF" / "templates",
+            Seq.empty,  /* default imports should be added here */
+            Seq(
+              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
+            ),  /* add extra bindings here */
+            Some("templates")
+          )
+        )
+      }
     )
   ).enablePlugins(JettyPlugin)
 }
