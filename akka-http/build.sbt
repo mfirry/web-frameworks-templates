69dd546ebe419d2271709f0561b1492ca1e6cf83
Marco F <mfirry@gmail.com>
2016-11-01 19:29:30 +0100

    akka-http updated

--- akka-http/build.sbt
+++ akka-http/build.sbt
@@ -2,13 +2,12 @@ name := """akka-http-example"""
 
 version := "0.0.1-SNAPSHOT"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.11.8"
 
 val akkaHttpVersion = "2.4.11"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion,
-  "com.typesafe.akka" %% "akka-stream" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-experimental" % akkaHttpVersion,
   "com.typesafe.akka" %% "akka-http-spray-json-experimental" % akkaHttpVersion
 )
 
--- akka-http/src/main/scala/Main.scala
+++ akka-http/src/main/scala/Main.scala
@@ -2,34 +2,29 @@ package com.example
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.RouteResult.route2HandlerFlow
+import akka.stream.ActorMaterializer
+
+// import akka.http.scaladsl.server.Route
+// import akka.http.scaladsl.server.RouteResult.route2HandlerFlow
 
 import spray.json._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 
 import com.typesafe.config.ConfigFactory
 
-import akka.stream.ActorMaterializer
-
 import spray.json.DefaultJsonProtocol._
-
-import akka.stream.scaladsl._
-import scala.concurrent._
 
 object Main extends App with SprayJsonSupport {
   val config = ConfigFactory.load()
 
   implicit val system = ActorSystem.create()
-
   implicit val executionContext = system.dispatcher
   implicit val materializer = ActorMaterializer()
 
   lazy val route =
     get {
-      path("") {
+      pathSingleSlash {
         complete(List(1, 2, 3))
       } ~
       path(Segment) { string =>
