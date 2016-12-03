ec8a1a9dafad8731ab1de2286d8896fbecdc03ea
David Denton <denton.david@gmail.com>
2016-12-03 22:03:17 +0000

    Adding fintrospect example

---
+++ fintrospect/src/main/scala/Echo.scala
@@ -0,0 +1,16 @@
+import com.twitter.finagle.Service
+import com.twitter.finagle.http.Method.Get
+import com.twitter.finagle.http.Request
+import com.twitter.finagle.http.Status.Ok
+import io.fintrospect.RouteSpec
+import io.fintrospect.formats.PlainText.ResponseBuilder.implicits._
+import io.fintrospect.parameters.Path
+
+object Echo {
+  def echo(value: String) = Service.mk { _: Request => Ok(value) }
+
+  val messageParam = Path.string("value")
+  val contract = RouteSpec("echo", "echoes the path string").at(Get) / "echo" / messageParam
+
+  val route = contract bindTo echo
+}
