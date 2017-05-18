f994085015193d57944a00cd06c1f406a5991d5a
Marco F <mfirry@users.noreply.github.com>
2017-05-18 22:45:58 +0200

    Analogweb update

--- analogweb/src/main/scala/com/example/Hello.scala
+++ analogweb/src/main/scala/com/example/Hello.scala
@@ -8,6 +8,14 @@ object Hello extends Analogweb {
   // default port is 8000
   def main(args: Array[String]) = Servers.run()
 
+  case class Message(message: String)
+
+  get("/json") {
+    Ok(asJson(Message("Hello, World!")))
+  }
+  get("/plaintext") {
+    "Hello, World!"
+  }
   get("/") {
     Ok(asJson(List(1,2,3)))
   }
