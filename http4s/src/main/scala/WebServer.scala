0a9ce955ef2904ad646c2736ccc2514447b3669a
Marco F <mfirry@users.noreply.github.com>
2017-12-11 13:31:32 +0100

    http4s: 0.18.0-M5

--- http4s/src/main/scala/WebServer.scala
+++ http4s/src/main/scala/WebServer.scala
@@ -1,22 +1,39 @@
-import org.http4s._
-import org.http4s.server._
-import org.http4s.dsl._
+import cats.effect._
+import fs2._
+import org.http4s.HttpService
+import org.http4s.dsl.Http4sDsl
+import org.http4s.server.Router
+import org.http4s.server.blaze.BlazeBuilder
+import org.http4s.util.{ExitCode, StreamApp}
 import _root_.argonaut._, Argonaut._
 import org.http4s.argonaut._
 
-import org.http4s.server.blaze.BlazeBuilder
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContext
 
-object WebServer extends App {
+object WebServerExample extends WebServerApp[IO]
 
-  val service = HttpService {
-    case GET -> Root =>
-      Ok((List(1, 2, 3)).asJson)
-    case GET -> Root / str =>
-      Ok(str.asJson)
-  }
-
-  BlazeBuilder.bindHttp(8080)
-    .mountService(service, "/")
-    .run
-    .awaitShutdown()
+class WebServerApp[F[_] : Effect] extends StreamApp[F] {
+  override def stream(args: List[String], requestShutdown: F[Unit]): Stream[F, ExitCode] =
+    Scheduler(corePoolSize = 2).flatMap { implicit scheduler =>
+      BlazeBuilder[F].bindHttp(8080)
+        .mountService(new ExampleService[F].service, "/")
+        .serve
+    }
 }
+
+class ExampleService[F[_]](implicit F: Effect[F]) extends Http4sDsl[F] {
+  def service(
+               implicit scheduler: Scheduler,
+               executionContext: ExecutionContext = ExecutionContext.global): HttpService[F] =
+    Router[F](
+      "" -> HttpService[F] {
+        case GET -> Root =>
+          Ok((List(1, 2, 3)).asJson)
+        case GET -> Root / str =>
+          Ok(str.asJson)
+      }
+    )
+
+}
+
