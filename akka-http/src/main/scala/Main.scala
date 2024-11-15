afe6f728f5b510b3e86ea4143a65856f018f8bd1
Marco F <mfirry@users.noreply.github.com>
2024-11-15 09:36:14 +0100

    akka: minor

--- akka-http/src/main/scala/Main.scala
+++ akka-http/src/main/scala/Main.scala
@@ -2,11 +2,8 @@ package com.example
 
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
-<<<<<<< HEAD
 import akka.stream.scaladsl._
 
-=======
->>>>>>> 22f2f4c (akka-http upgrade #«)
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
@@ -29,12 +26,6 @@ object Main extends App with SprayJsonSupport {
     implicit val messageFormat = jsonFormat1(Message)
   }
 
-<<<<<<< HEAD
-=======
-  implicit val system = ActorSystem(Behaviors.empty, "my-system")
-  implicit val executionContext = system.executionContext
-
->>>>>>> 22f2f4c (akka-http upgrade #«)
   import MessageProtocol._
 
   lazy val route =
@@ -55,7 +46,6 @@ object Main extends App with SprayJsonSupport {
         }
     }
 
-<<<<<<< HEAD
   import scala.io.StdIn
   val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
@@ -63,13 +53,5 @@ object Main extends App with SprayJsonSupport {
   bindingFuture
     .flatMap(_.unbind()) // trigger unbinding from the port
     .onComplete(_ => system.terminate()) // and shutdown when done
-=======
-  val bindingFuture =
-    Http().newServerAt(interface = "localhost", port = 9000).bind(route)
-
-  bindingFuture
-    .flatMap(_.unbind())
-    .onComplete(_ => system.terminate())
->>>>>>> 22f2f4c (akka-http upgrade #«)
 
 }
